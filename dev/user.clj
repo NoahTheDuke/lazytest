fe3ea99392f4582069fbaf1ad13f219a42dc2928
Noah Bogart <noah.bogart@hey.com>
2023-08-29 23:26:00 -0400

    Add :dev alias and dev/user.clj

---
+++ dev/user.clj
@@ -0,0 +1,27 @@
+(ns user
+  (:require
+    [clj-java-decompiler.core :as decompiler]
+    [clojure.main :as main]
+    [criterium.core :as criterium]))
+
+(set! *warn-on-reflection* true)
+
+; (def repl-requires
+;   "A sequence of lib specs that are applied to `require`
+;   by default when a new command-line REPL is started."
+;   '[[clojure.repl :refer (source apropos dir pst doc find-doc)]
+;     [clojure.java.javadoc :refer (javadoc)]
+;     [clojure.pprint :refer (pp pprint)]])
+(apply require main/repl-requires)
+
+(defmacro decompile
+  "Decompile the form into Java and print it to stdout. Form shouldn't be quoted."
+  [form]
+  `(decompiler/decompile ~form))
+
+(defmacro quick-bench
+  "Convenience macro for benchmarking an expression, expr.  Results are reported
+  to *out* in human readable format. Options for report format are: :os,
+  :runtime, and :verbose."
+  [expr & opts]
+  `(criterium/quick-bench ~expr ~@opts))

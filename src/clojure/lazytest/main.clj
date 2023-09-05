613270eeab1ebd6cffa73fe171990f34388795cd
Noah Bogart <noah.bogart@hey.com>
2023-09-05 10:45:18 -0400

    Split -main into subfunction

--- src/clojure/lazytest/main.clj
+++ src/clojure/lazytest/main.clj
@@ -3,30 +3,41 @@
   (:gen-class)
   (:require
    [clojure.java.io :as io]
+   [clojure.tools.namespace.file :refer [read-file-ns-decl]]
    [lazytest.cli :refer [validate-opts]]
    [lazytest.report.console :as console]
    [lazytest.report.nested :as nested]
    [lazytest.report.summary :as summary]
    [lazytest.results :refer [summarize summary-exit-value]]
    [lazytest.runner :refer [run-tests]]
-   [lazytest.tracker :refer [tracker]]))
+   [lazytest.tracker :refer [find-sources]]))
+
+(defn find-ns-decls [dirs]
+  (mapv second (keep read-file-ns-decl (find-sources dirs))))
+
+(defn require-dirs [dir]
+  (let [dirs (map io/file dir)
+        nses (find-ns-decls dirs)]
+    (apply require nses)
+    nses))
+
+(defn run [{:keys [dir output]}]
+  (let [nses (require-dirs dir)
+        results (apply run-tests nses)
+        summary (summarize results)]
+    (case output
+      "console" (console/report results)
+      "nested" (nested/report results)
+      nil)
+    (summary/report results)
+    (System/exit (summary-exit-value summary))))
 
 (defn -main
   "Run with directories as arguments. Runs all tests in those
   directories; returns 0 if all tests pass."
   [& args]
-  (let [{:keys [dir output
-                exit-message ok]} (validate-opts args)]
-    (when exit-message
-      (println exit-message)
-      (System/exit (if ok 0 1)))
-    (let [namespaces ((tracker (map io/file dir) 0))
-          _ (apply require namespaces)
-          results (apply run-tests namespaces)
-          summary (summarize results)]
-      (case output
-        "console" (console/report results)
-        "nested" (nested/report results)
-        nil)
-      (summary/report results)
-      (System/exit (summary-exit-value summary)))))
+  (let [{:keys [exit-message ok] :as opts} (validate-opts args)]
+    (if exit-message
+      (do (println exit-message)
+          (System/exit (if ok 0 1)))
+      (run opts))))
